b53b138782d26682636e47d08e15783ae14a73f0
ashaytiwari <ashaytiwari2001@gmail.com>
2026-06-08 14:30:00 +0530

    chore: setup sqlite and typeorm, initialize db in app module

--- my-car-value/src/app.module.ts
+++ my-car-value/src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'my-car-value.db',
+      entities: [],
+      synchronize: true,
+      logging: true
+    }),
+    UsersModule,
+    ReportsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
