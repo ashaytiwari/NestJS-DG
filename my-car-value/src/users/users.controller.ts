fe11fee85a0891218e057cabdcac9b3930494eb2
ashaytiwari <ashaytiwari2001@gmail.com>
2026-06-13 13:33:40 +0530

    feat: implemented other crud operations for user module

--- my-car-value/src/users/users.controller.ts
+++ my-car-value/src/users/users.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete } from '@nestjs/common';
 
 import { UsersService } from './users.service';
+
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -9,8 +11,33 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Post('/signup')
-  async create(@Body() body: CreateUserDto) {
+  create(@Body() body: CreateUserDto) {
     this.usersService.create(body.firstName, body.lastName, body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUserById(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get('/users')
+  findAllUsers() {
+    return this.usersService.findAll();
+  }
+
+  @Get()
+  findUserByEmail(@Query('email') email: string) {
+    return this.usersService.findByEmail(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 
 }
--- my-car-value/src/users/users.service.ts
+++ my-car-value/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -12,6 +12,48 @@ export class UsersService {
   create(firstName: string, lastName: string, email: string, password: string) {
     const user = this.repo.create({ firstName, lastName, email, password });
     return this.repo.save(user);
+  }
+
+  async findOne(id: number) {
+    const user = await this.repo.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
+
+  async findAll() {
+    const users = await this.repo.find({});
+    return users;
+  }
+
+  findByEmail(email: string) {
+    return this.repo.findOne({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+
+    const user = await this.repo.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+
+  }
+
+  async remove(id: number) {
+    const user = await this.repo.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return this.repo.remove(user);
   }
 
 }
